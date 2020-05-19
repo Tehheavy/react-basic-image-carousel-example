3f4b3a63141914b361ab423e5936af3cdfb499b6
Alex Dushevski <39350701+Tehheavy@users.noreply.github.com>
2020-05-20 00:35:13 +0300

    Updates

---
+++ precache-manifest.98d5a59aedff2fe53fbaf0a1abd55b0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ef59712552c7a6bd5ade2452dedf5c9",
+    "url": "/react-basic-image-carousel-example/index.html"
+  },
+  {
+    "revision": "6d13c4efac2464251c91",
+    "url": "/react-basic-image-carousel-example/static/css/main.eb53f6d8.chunk.css"
+  },
+  {
+    "revision": "90159c9556b439dfbaba",
+    "url": "/react-basic-image-carousel-example/static/js/2.67819857.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-basic-image-carousel-example/static/js/2.67819857.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d13c4efac2464251c91",
+    "url": "/react-basic-image-carousel-example/static/js/main.c1ff7306.chunk.js"
+  },
+  {
+    "revision": "559b12c4d1480bbebc0e",
+    "url": "/react-basic-image-carousel-example/static/js/runtime-main.48dda265.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-basic-image-carousel-example/static/media/logo.5d5d9eef.svg"
+  }
+]);
